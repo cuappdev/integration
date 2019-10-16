1298d0d94a602b39b2eddcae2fe4eae72fc07bc3
Sean Corcoran <sdc76@cornell.edu>
2019-10-15 23:18:43 -0400

    clean up JSON

--- src/eatery/tests.py
+++ src/eatery/tests.py
@@ -10,7 +10,7 @@ URL_PARAMS_CAMPUS_NON_EMPTY = "?query=" + parse.quote(  # url encoding
     """{
   campusEateries {
     about
-    campusArea{
+    campusArea {
       descriptionShort
     }
     coordinates {
