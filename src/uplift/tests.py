5e8477892df9d8ed51b3721199562fd11dbeb7eb
Sean Corcoran <31456107+seancorc@users.noreply.github.com>
2019-10-23 18:01:34 -0400

    Add two test cases for uplift (#17)
    Added two tests (a query for gyms and class info) to check that the fields which should always have a value indeed do.

--- src/uplift/tests.py
+++ src/uplift/tests.py
@@ -4,8 +4,64 @@ from urllib import parse
 from models import Application, Request, Test, TestGroup
 
 BASE_URL = environ["UPLIFT_BACKEND_URL"]
-URL_PARAMS = "?query=" + parse.quote("query { gyms { name } }")  # url encoding
+URL_PARAMS_BASIC = "?query=" + parse.quote("query { gyms { name } }")  # url encoding
+URL_PARAMS_GYMS_NON_EMPTY = "?query=" + parse.quote(  # url encoding
+    """
+    query GymsQuery {
+        gyms {
+            facilities {
+                equipment {
+                    name
+                }
+                name
+                times {
+                    day
+                    endTime
+                    startTime
+                    restrictions
+                }
+            }
+            name
+        }
+    }
+    """
+)
+URL_PARAMS_CLASSES_NON_EMPTY = "?query=" + parse.quote(  # url encoding
+    """
+    query ClassesInfoQuery {
+        classes {
+            date
+            endTime
+            isCancelled
+            startTime
+        }
+    }
+        """
+)
 
-tests = [Test(name="Gyms on Campus query", request=Request(method="GET", url=BASE_URL + URL_PARAMS))]
+
+def all_gym_fields_non_empty(r):
+    response = r.json()
+    return all(response["data"]["gyms"])
+
+
+def all_classes_fields_non_empty(r):
+    response = r.json()
+    return all(response["data"]["classes"])
+
+
+tests = [
+    Test(name="Gyms on Campus query", request=Request(method="GET", url=BASE_URL + URL_PARAMS_BASIC)),
+    Test(
+        name="Fields that should never be empty for gyms query",
+        request=Request(method="GET", url=BASE_URL + URL_PARAMS_GYMS_NON_EMPTY),
+        callback=all_gym_fields_non_empty,
+    ),
+    Test(
+        name="Fields that should never be empty for classes info query",
+        request=Request(method="GET", url=BASE_URL + URL_PARAMS_CLASSES_NON_EMPTY),
+        callback=all_classes_fields_non_empty,
+    ),
+]
 
 uplift_tests = TestGroup(application=Application.UPLIFT, name="Uplift", tests=tests)
