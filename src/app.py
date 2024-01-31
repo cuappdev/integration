59eb71d9438a25cdc55f7811d654fe991cacbe15
Kidus Zegeye <kidzegeye@gmail.com>
2024-01-31 15:16:53 -0500

    temporarily disable endpoint

--- src/app.py
+++ src/app.py
@@ -4,10 +4,10 @@ from main import run_tests
 
 app = Flask(__name__)
 
-@app.route("/run_tests", methods=["POST"])
+# @app.route("/run_tests", methods=["POST"])
 def run():
     run_tests()
-    
+
 
 if __name__ == '__main__':
     app.run(port=8080)
