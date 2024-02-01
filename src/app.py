37c9608545acdec92e28e6170187e3497e233128
Kidus Zegeye <kidzegeye@gmail.com>
2024-01-31 22:11:26 -0500

    Remove endpoint

--- src/app.py
+++ src/app.py
@@ -7,14 +7,8 @@ app = Flask(__name__)
 
 @app.route("/integration/")
 def hello_world():
-    return "<h3>Hello World!</h3>"
-
-@app.route("/integration/run_tests/", methods=["POST"])
-def run():
-    run_tests(["--use-config"])
     with open(path.join(environ['BASE_DIR'], 'config/test_config.txt'), 'r') as file:
         return file.read()
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=False)
