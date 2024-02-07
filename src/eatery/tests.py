6552724edd357c1269c70b7c63de9d1a280a710b
Kidus Zegeye <kidzegeye@gmail.com>
2024-02-07 10:21:36 -0500

    Change eatery tests to eatery blue

--- src/eatery/tests.py
+++ src/eatery/tests.py
@@ -5,71 +5,19 @@ from models import Application, Request, Test, TestGroup
 
 
 BASE_URL = environ["EATERY_BACKEND_URL"]
-URL_PARAMS_BASIC = "?query=" + parse.quote("query { eateries { name } }")  # url encoding
-URL_PARAMS_CAMPUS_NON_EMPTY = "?query=" + parse.quote(  # url encoding
-    """{
-  campusEateries {
-    about
-    campusArea {
-      descriptionShort
-    }
-    coordinates {
-      latitude
-      longitude
-    }
-    location
-    name
-    nameShort
-    paymentMethodsEnums
-    slug
-  }
-}
-"""
-)
-URL_PARAMS_COLLEGETOWN_NON_EMPTY = "?query=" + parse.quote(  # url encoding
-    """{
-  collegetownEateries {
-    address
-    categories
-    coordinates {
-      latitude
-      longitude
-    }
-    eateryType
-    id
-    name
-    paymentMethodsEnums
-    phone
-    price
-    ratingEnum
-    url
-  }
-}
-"""
-)
-
-
-def all_campus_fields_non_empty(r):
-    response = r.json()
-    return all(response["data"]["campusEateries"])
-
-
-def all_collegetown_fields_non_empty(r):
-    response = r.json()
-    return all(response["data"]["collegetownEateries"])
-
+SIMPLE_EATERY_QUERY = "eatery/simple/"
+EATERY_QUERY_BY_ID = "eatery/5/"
+FULL_EATERY_QUERY = "eatery/"
 
 tests = [
-    Test(name="Eateries on campus query", request=Request(method="GET", url=BASE_URL + URL_PARAMS_BASIC)),
+    Test(name="Simple eateries query", request=Request(method="GET", url=BASE_URL + SIMPLE_EATERY_QUERY)),
     Test(
-        name="Fields that should never be empty for campus eateries query",
-        request=Request(method="GET", url=BASE_URL + URL_PARAMS_CAMPUS_NON_EMPTY),
-        callback=all_campus_fields_non_empty,
+        name="Get eatery by id",
+        request=Request(method="GET", url=BASE_URL + EATERY_QUERY_BY_ID)
     ),
     Test(
-        name="Fields that should never be empty for collegetown eateries query",
-        request=Request(method="GET", url=BASE_URL + URL_PARAMS_COLLEGETOWN_NON_EMPTY),
-        callback=all_collegetown_fields_non_empty,
+        name="Get all eatery data",
+        request=Request(method="GET", url=BASE_URL + FULL_EATERY_QUERY),
     ),
 ]
 
