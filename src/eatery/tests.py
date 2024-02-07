fec1182b1c50639bb7d0f4ea88bc6a20b5e038cf
Kidus Zegeye <kidzegeye@gmail.com>
2024-02-07 15:31:44 -0500

    Remove eatery test

--- src/eatery/tests.py
+++ src/eatery/tests.py
@@ -7,17 +7,12 @@ from models import Application, Request, Test, TestGroup
 BASE_URL = environ["EATERY_BACKEND_URL"]
 SIMPLE_EATERY_QUERY = "eatery/simple/"
 EATERY_QUERY_BY_ID = "eatery/5/"
-FULL_EATERY_QUERY = "eatery/"
 
 tests = [
     Test(name="Simple eateries query", request=Request(method="GET", url=BASE_URL + SIMPLE_EATERY_QUERY)),
     Test(
         name="Get eatery by id",
         request=Request(method="GET", url=BASE_URL + EATERY_QUERY_BY_ID)
-    ),
-    Test(
-        name="Get all eatery data",
-        request=Request(method="GET", url=BASE_URL + FULL_EATERY_QUERY),
     ),
 ]
 
